2ff96fdb61b0623c16faa3df22ae147c306a712d
obna <obn.adeleke@gmail.com>
2025-02-24 20:00:52 -0500

    feat: begin state management

--- given/test/src/pages/Home.tsx
+++ given/test/src/pages/Home.tsx
@@ -1,23 +1,26 @@
 //
 import React from "react";
+import { useState } from 'react';
 import { List, ListItem, Avatar, ListItemButton, Button, Typography } from "@mui/material";
 
 function Home() {
-  let msg = "hello ello lo";
+  let msg = "click me";
   let name = "chad";
-  let cities = ['chicago', 'aspen', 'denver', 'seattle' ]
-  let countries = [1, 2, 3, 4]
+  let cities = ['chicago', 'aspen', 'denver', 'seattle'];
+  let [counter,  setCounter] = useState(0);
   
   const handleClick = (event : React.MouseEvent<HTMLButtonElement>) => {
-    console.log('clicked')
-    console.log(event.timeStamp)
+    console.log('was clicked')
+    setCounter(counter + 1);
+    console.log(counter);
   }
 
   return (
     <>
       <Button variant="outlined" onClick={handleClick} > {msg}</Button>
-      
-      {name ? (<Typography variant='h3'> Hello {name} </Typography>) : (<Typography variant="h1">{msg}</Typography>)}
+      {/* <Typography variant='h3'> Counter {counter} </Typography> */}
+      {counter > 3 ? <Typography> is {"\<"} 3: {counter} </Typography> : <Typography> is {">"} 3: {counter} </Typography> }
+      {/* {name ? (<Typography variant='h3'> Hello {name} </Typography>) : (<Typography variant="h1">{msg}</Typography>)}
       
       <List>
         {cities.map((city) => (<ListItem key={city}> {city} </ListItem>))}
@@ -30,7 +33,7 @@ function Home() {
           </>
         ))}
         {cities.map((city) => (<ListItemButton><Typography variant="subtitle1">{city}</Typography></ListItemButton>)) }
-      </List>
+      </List> */}
 
     </>
   );
