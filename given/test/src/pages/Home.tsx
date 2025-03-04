90dcad7f6c4cb634c4132e80b523d7c2c8e4a020
obna <obn.adeleke@gmail.com>
2025-03-03 20:53:36 -0500

    Revert "feat: begin state management"
    This reverts commit 2ff96fdb61b0623c16faa3df22ae147c306a712d.

--- given/test/src/pages/Home.tsx
+++ given/test/src/pages/Home.tsx
@@ -1,26 +1,23 @@
 //
 import React from "react";
-import { useState } from 'react';
 import { List, ListItem, Avatar, ListItemButton, Button, Typography } from "@mui/material";
 
 function Home() {
-  let msg = "click me";
+  let msg = "hello ello lo";
   let name = "chad";
-  let cities = ['chicago', 'aspen', 'denver', 'seattle'];
-  let [counter,  setCounter] = useState(0);
+  let cities = ['chicago', 'aspen', 'denver', 'seattle' ]
+  let countries = [1, 2, 3, 4]
   
   const handleClick = (event : React.MouseEvent<HTMLButtonElement>) => {
-    console.log('was clicked')
-    setCounter(counter + 1);
-    console.log(counter);
+    console.log('clicked')
+    console.log(event.timeStamp)
   }
 
   return (
     <>
       <Button variant="outlined" onClick={handleClick} > {msg}</Button>
-      {/* <Typography variant='h3'> Counter {counter} </Typography> */}
-      {counter > 3 ? <Typography> is {"\<"} 3: {counter} </Typography> : <Typography> is {">"} 3: {counter} </Typography> }
-      {/* {name ? (<Typography variant='h3'> Hello {name} </Typography>) : (<Typography variant="h1">{msg}</Typography>)}
+      
+      {name ? (<Typography variant='h3'> Hello {name} </Typography>) : (<Typography variant="h1">{msg}</Typography>)}
       
       <List>
         {cities.map((city) => (<ListItem key={city}> {city} </ListItem>))}
@@ -33,7 +30,7 @@ function Home() {
           </>
         ))}
         {cities.map((city) => (<ListItemButton><Typography variant="subtitle1">{city}</Typography></ListItemButton>)) }
-      </List> */}
+      </List>
 
     </>
   );
