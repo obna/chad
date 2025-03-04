d1301b7309f311340284c5880d8105f15135ffc5
obna <obn.adeleke@gmail.com>
2025-03-03 21:07:17 -0500

    feat: add db

--- given/test/src/pages/Explore.tsx
+++ given/test/src/pages/Explore.tsx
@@ -1,12 +1,43 @@
-
-
-
-
 import React from 'react'
+import { Typography } from '@mui/material'
+
+
+import { db } from "../config/firebase.ts"
+import { doc, getDoc } from "firebase/firestore"
+import { useEffect } from 'react'
+
+interface Principal{
+  district: string;
+  firstname: string;
+  lastname: string;
+  uid: number;
+}
 
 function Explore() {
+
+  useEffect(() => {
+    const fetchPrincipal = async () => {
+      const docRef = doc(db, "principal", "jholmes")
+      const docData = await getDoc(docRef);
+
+      if (docData.exists()) {
+        const principalData = docData.data() as Principal;
+        console.log(principalData)
+        return principalData;
+      } else {
+        console.log('No such document')
+      }
+      console.log(docRef)
+    };
+    fetchPrincipal();
+  })
+    
   return (
-    <div>Explore</div>
+    <>
+    
+      <Typography variant="h1">principal</Typography>
+    
+    </>
   )
 }
 
